1525659fb594b2b7a8004abf79ba9e237eebbe46
polar555 <garyworkzht@gmail.com>
2024-06-26 18:26:38 -0700

    Add secondary servers feature.

--- app.py
+++ app.py
@@ -1,10 +1,56 @@
 from flask import Flask, request, jsonify
 import os
 import socket
+import concurrent.futures
+import urllib.request
+import json
 
 app = Flask(__name__)
-LOG_DIR = './data' #/var/log'
+LOG_DIR = '/var/log'
+PRIMARY = False
+THREAD_POOL_MAX_WORKERS = 10
+SECONDARY_SERVERS = 'second.cfg'
 # MAX_PER_PAGE = 100
+
+def get_remote_log(filename, keyword, limit, offset):
+    def send_req(hostname):
+        # construct secondary URL
+        url = f"http://{hostname}:5000/logs?filename={filename}"
+        if keyword:
+            url += f"&keyword={keyword}"
+
+        if limit:
+            url += f"&limit={limit}"
+
+        if offset:
+            url += f"&offset={offset}"
+
+        with urllib.request.urlopen(url, timeout=5) as conn:
+            return conn.read()
+        
+    # load secondary configs
+    if not os.path.exists(SECONDARY_SERVERS):
+        app.logger.warning(f"Config file for Secondary servers is not exist")
+        return []
+
+    with open(SECONDARY_SERVERS, 'r') as file:
+        hosts = file.readlines()
+    
+    # use threadpool to retrieve logs from seconary servers
+    res = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=THREAD_POOL_MAX_WORKERS) as excutor:
+        futures = {excutor.submit(send_req, hostname.strip()): hostname.strip() for hostname in hosts}
+        for future in concurrent.futures.as_completed(futures, timeout=10):
+            try:
+                data = future.result()
+                res.append(json.loads(data)[0])
+            except Exception as e:
+                app.logger.warning(f"Failed to get log from secondary server ({futures[future]}. Exception: {str(e)})")
+            else:
+                app.logger.info(f"Retrieved logs from secondary server ({futures[future]}) with file {filename}")
+
+    return res
+
 
 @app.route('/logs', methods=['GET'])
 def get_logs():    
@@ -16,10 +62,12 @@ def get_logs():
     app.logger.debug(f"filename={filename}, keyword={keyword}, limit={limit}, offset={offset}")
 
     if not filename:
+        app.logger.error('No filename specified')
         return jsonify({'error': 'No filename specified'}), 400
 
     file_path = os.path.join(LOG_DIR, filename)
     if not os.path.exists(file_path):
+        app.logger.error('No filename specified')
         return jsonify({'error': 'No specified file'}), 404
 
     lines = []
@@ -31,6 +79,7 @@ def get_logs():
 
                 lines.append(line.strip())
     except Exception as e:
+        app.logger.error(str(e))
         return jsonify({'error': str(e)}), 500
 
     end = len(lines) - offset
@@ -50,5 +99,8 @@ def get_logs():
         'offset': offset + limit,
         'log': lines
         }]
+    
+    if PRIMARY:
+        res.extend(get_remote_log(filename, keyword, limit, offset))
 
     return jsonify(res)
